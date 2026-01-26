0069e384361189c22db8af14d6d2c614e0261e78
G-MTI <gaia.mazzanti07@gmail.com>
2026-01-26 01:24:41 +0100

    update

--- function.py
+++ function.py
@@ -2,7 +2,13 @@
 import pandas as pd
 import yfinance as yf
 
-def get_stock_data(tickers_input, start_input, end_input):
-    data = yf.download(tickers=tickers_input, start=start_input, end=end_input)
+#start_date=start_input-200
+#end_date=end_input
+
+def get_stock_data(tickers_input, start_date, end_date):
+    data = yf.download(tickers=tickers_input, start=start_date, end=end_date)
     return data
+
+def sma_calculate(data, slow_input, fast_input):
+    data=data.copy()
 
